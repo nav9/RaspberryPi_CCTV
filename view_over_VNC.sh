1ff29804378e29bef09621d365700867283fe923
Nav <navinipe@gmail.com>
2025-06-28 19:28:27 +0530

    Update view_over_VNC.sh
    path made generic

--- view_over_VNC.sh
+++ view_over_VNC.sh
@@ -2,9 +2,11 @@
 #This script plays the video stream from the webcam using VLC and this can be viewed from any 
 #other device by connecting via VNC (although no audio will be available over free versions of VNC)
 
+CURRENT_DIR=$(pwd)
+DEVICE_DETECTOR_SCRIPT="$CURRENT_DIR/device_detector.py"
 # Auto-detect devices using your detection script since sometimes the endpoint number varies when connecting to a different USB port
-VIDEO_DEV=$(python3 /home/nav/RaspberryPi_CCTV/device_detector.py video)
-AUDIO_DEV=$(python3 /home/nav/RaspberryPi_CCTV/device_detector.py audio)
+VIDEO_DEV=$(python3 "$DEVICE_DETECTOR_SCRIPT" video)
+AUDIO_DEV=$(python3 "$DEVICE_DETECTOR_SCRIPT" audio)
 
 # Fall back to defaults if detection fails
 [[ -z "$VIDEO_DEV" || ! -e "$VIDEO_DEV" ]] && VIDEO_DEV="/dev/video0"
